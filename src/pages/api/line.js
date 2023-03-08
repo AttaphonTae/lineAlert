62e2ae773623116a08f7cd5eb8364c1fd201f3e9
Tksm-Attaphon <attaphon@thinksmart.co.th>
2023-03-08 20:02:39 +0700

    some api

---
+++ src/pages/api/line.js
@@ -0,0 +1,37 @@
+
+import { Client } from "@line/bot-sdk";
+
+const client = new Client({
+  channelAccessToken: process.env.LINE_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_CHANNEL_SECRET,
+});
+
+export default function handler(req, res) {
+    const events = req.body.events;
+    if (!events || events.length === 0) {
+        // No events to handle
+        res.status(200).end();
+        return;
+      }
+  const message = {
+    type: "text",
+    text: "hello world",
+  };
+
+  Promise.all(
+    events.map((event) => {
+      return client.replyMessage(event.replyToken, message);
+    })
+  )
+    .then(() => {
+      res.status(200).end();
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+
+  // Handle LINE events here
+
+  res.status(200).end();
+}
---
+++ src/pages/home/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>hello world this is home page</div>
+  )
+}
